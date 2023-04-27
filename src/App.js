eaa5da5f8ff18d22cccf47983d99ce2246233597
sani-coder <saneeppp@gmail.com>
2023-04-27 13:00:54 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const resultfun = () => {
+    let sr = document.getElementById("input").value;
+    if (sr === "" || sr === null) {
+      alert("Input Should not Empty...!");
+    } else {
+      let c = wordCount(sr);
+      alert(`Total Number of words in this paragraph is ${c}`);
+    }
+  };
+  const wordCount = (s) => {
+    return s.split(" ").length;
+  };
+  const clearfun = () => {
+    document.getElementById("input").value = "";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Word- Count</h2>
+      <div className="container">
+        <div className="box">
+          <textarea
+            id="input"
+            rows="4"
+            cols="60"
+            placeholder="Paste the Paragraph Here...."
+          ></textarea>
+        </div>
+        <button id="btn" type="button" onClick={resultfun}>
+          Count
+        </button>
+        <button id="btn1" type="button" onClick={clearfun}>
+          Clear
+        </button>
+      </div>
+    </>
   );
 }
 
